263ba79e115e5dd71e884b1634e9a709f9886727
daveman235 <awad.david@gmail.com>
2020-07-14 20:22:35 -0400

    commiting before PR

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,14 @@
-var backgroundColor = document.body.style.background = "orange";
+let backgroundColor = document.body.style.background = "orange";
 addElement();
 
 
 function addElement() {
-    var newHeading = document.createElement('h1');
+    let newHeading = document.createElement('h1');
 
-    var newContent = document.createTextNode("Hello World!");
+    let newContent = document.createTextNode("Hello World!");
 
     newHeading.appendChild(newContent);
 
-    var originalHeading = document.getElementsByTagName('h1')[0];
+    let originalHeading = document.getElementsByTagName('h1')[0];
     document.body.insertBefore(newHeading, originalHeading);
 }
